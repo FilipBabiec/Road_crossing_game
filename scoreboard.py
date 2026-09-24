6b975b5dc9fe8715e88b2971e74abd846ce7bf52
Filip Babiec <filipbabiec@gmail.com>
2026-09-24 16:42:46 +0200

    Added high score mechanics

--- scoreboard.py
+++ scoreboard.py
@@ -12,18 +12,38 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.goto(-290, 260)
         self.pendown()
-        self.write(f"Current level: {self.score}", font=("Arial",20,"normal"))
+        with open("high_score.txt") as file:
+            self.highscore = int(file.read())
+        self.update_scoreboard()
 
     def raise_level(self):
         self.score += 1
-        self.clear()
-        self.write(f"Current level: {self.score}", font=("Arial",20,"normal"))
+        self.update_scoreboard()
 
     def end_game(self, screen):
-        screen.update()  # draw the final frame so the collision is visible behind the popup
-        messagebox.showinfo(
-            title="Game over",
-            message=f"You got hit!\n\nLevel reached: {self.score}",
-            parent=screen.getcanvas().winfo_toplevel(),
-        )
-        screen.bye()  # close the game window after OK
+        screen.update()
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("high_score.txt", mode="w") as f:
+                f.write(str(self.highscore))
+
+            messagebox.showinfo(
+                title="Game over",
+                message=f"You got hit!\n\nYou have hit a new high score!\n\nLevel reached: {self.score}",
+                parent=screen.getcanvas().winfo_toplevel(),
+            )
+            self.score = 0
+            screen.bye()
+
+        else:
+            messagebox.showinfo(
+                title="Game over",
+                message=f"You got hit!\n\nLevel reached: {self.score}",
+                parent=screen.getcanvas().winfo_toplevel(),
+            )
+            self.score = 0
+            screen.bye()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Current level: {self.score}    High score: {self.highscore}", font=("Arial", 20, "normal"))
